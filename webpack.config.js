db42cd26e5468236d19ba573bfbb1978e51eabe5
Christos Petrou <christosptr@gmail.com>
2020-01-31 12:31:45 +1100

    Lesson 15

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path')
+const postCSSPlugings = [
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
 
 module.exports = {
     entry: './app/assets/scripts/App.js',
@@ -7,5 +12,11 @@ module.exports = {
         path: path.resolve(__dirname, 'app')
     },
     mode: 'development',
-    watch: true
+    watch: true,
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader?url=false', { loader: 'postcss-loader', options: { plugins: postCSSPlugings } }]
+        }]
+    }
 }
